7a8bc5a9c1a1a396978a6cb87c5467f33a80cc6b
baarnold28 <baarnold@udel.edu>
2024-09-02 12:15:27 -0400

    Update HelloWorld.cs

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    Debug.Log("Hello,world!");    
+    Debug.Log("Hello,world!");
+    Debug.LogWarning("Do not push!");
+    Debug.LogError("This Will Not Work!");
+    //"Author:"Brandon Arnold
+    /* Wanting to design a Medievil type of game, Hope to learn about the development of original games and new
+    */
     }
 
     // Update is called once per frame
