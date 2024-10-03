acb93243c3831bbce7a42b68562adf4541be1471
baarnold28 <baarnold@udel.edu>
2024-10-03 11:26:53 -0400

    HW1

---
+++ HomeWork Project Old/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    Debug.Log("Hello,world!");
+    Debug.LogWarning("Do not push!");
+    Debug.LogError("This Will Not Work!");
+    //"Author:"Brandon Arnold
+    /* Wanting to design a Medievil type of game, Hope to learn about the development of original games and new
+    */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
