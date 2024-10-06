583c421dcdd11ecc7f79b397444627319aac8cb5
baarnold28 <baarnold@udel.edu>
2024-10-06 16:58:55 -0400

    t

---
+++ Turtle Race/Assets/Turtle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour
+{
+    //Write your variables here:
+    string turtleName = "turtle 1" ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Debug.Log("+turtleName is ready!"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
